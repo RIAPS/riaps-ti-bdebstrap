ddcb7dda3976675ffe7d8b178a21510bff3cd329
Mary Metelko <mary.a.metelko@vanderbilt.edu>
2025-02-26 10:18:06 -0600

    Updates based on changes in ti-bdebstrap for trixie release

--- build.sh
+++ build.sh
@@ -70,9 +70,9 @@ do
     echo "> Validate build ..."
     validate_build ${machine} ${bsp_version} ${distro_codename}/${distro}.yaml
 
-    generate_rootfs ${distro} ${distro_codename} ${machine}
+    generate_rootfs ${distro} ${distro_codename} ${machine} ${bsp_version}
     build_bsp ${distro} ${machine} ${bsp_version}
-    package_and_clean ${distro}
+    package_and_clean ${distro} ${bsp_version}
 
 done
 
